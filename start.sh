d070cf889bb8db9c35e02784707fe0e918e42aa9
Gergely Imreh <imrehg@gmail.com>
2015-08-11 22:01:53 +0800

    add software volume control

--- start.sh
+++ start.sh
@@ -2,7 +2,9 @@
 
 export CHANNEL=${CHANNEL:=groovesalad130}
 export CACHE=${CACHE:=2000}
+export VOLUME=${VOLUME:=100}
 
 mplayer -cache ${CACHE} \
         -quiet \
+	-volume $VOLUME \
         -playlist "http://somafm.com/${CHANNEL}.pls"
